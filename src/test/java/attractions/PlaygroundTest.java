f1191e5d0651098f851bac81dfc05f3f17c42038
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-23 15:47:14 +0000

    playground implements ISecurity & tests pass

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(8, 1.0, 20.50);
+        visitor2 = new Visitor(17, 1.6, 37.25);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void allows8YearOldIn(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void doesNotAllow17YearOldIn(){
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
