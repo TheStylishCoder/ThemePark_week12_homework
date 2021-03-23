b1a5d89cd68747e1882f2ddaa8af3b85a6bdbcbd
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-23 16:39:18 +0000

    added rating property to stall class & tests pass

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -14,7 +14,7 @@ public class TobaccoStallTest {
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 1);
         visitor1 = new Visitor(35, 1.8, 50.00);
         visitor2 = new Visitor(17, 1.6, 37.25);
     }
@@ -32,6 +32,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void hasRating(){
+        assertEquals(1, tobaccoStall.getRating());
     }
 
     @Test
